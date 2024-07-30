93b6986daab7fce8515bc31fa8ba50ad71ec3ae8
taka <gluestick829@gmail.com>
2024-07-30 23:28:46 +0900

    MainActivity.ktに、menuListにデータを追加する処理を実装

--- app/src/main/java/com/example/intentsample/MainActivity.kt
+++ app/src/main/java/com/example/intentsample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.intentsample
 
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lvMenu)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
+        var menu = mutableMapOf("name" to "唐揚げ定食", "price" to "800円")
+        menuList.add(menu)
+
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
+        menuList.add(menu)
+
+        val from = arrayOf("name", "price")
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        val adapter = SimpleAdapter(
+            this@MainActivity,
+            menuList,
+            android.R.layout.simple_list_item_2,
+            from,
+            to
+        )
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.adapter = adapter
     }
 }
